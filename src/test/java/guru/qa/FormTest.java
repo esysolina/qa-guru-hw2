bfcc7bf24465aaf05a45456cb95da3b48ce02823
esysolina <evelina.ova@gmail.com>
2021-12-19 00:15:56 +0500

    added check

--- src/test/java/guru/qa/FormTest.java
+++ src/test/java/guru/qa/FormTest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -20,14 +21,36 @@ public class FormTest {
         $("#lastName").setValue("Student");
         $("#userEmail").setValue("test@test.ru");
         $(byText("Female")).click();
-        $("#userNumber").setValue("89091055555");
-        $("#dateOfBirthInput").setValue("18 Apr 1990");
-        executeJavaScript("$('.datepicker').hide();");
+        $("#userNumber").setValue("8909105555");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__year-select").selectOption("1990");
+        $(".react-datepicker__day--018:not(.react-datepicker__day--outside-month").click();
+
         $("#subjectsInput").setValue("a");
-        $("#react-select-2-option-3").click();
+        $("#react-select-2-option-2").click();
+
         $(byText("Reading")).click();
         $("#uploadPicture").uploadFromClasspath("file.txt");
         $("#currentAddress").setValue("Test st., 6");
+
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
+
+        $x("//tr[1]/td[2]").shouldHave(text("Super Student"));
+        $x("//tr[2]/td[2]").shouldHave(text("test@test.ru"));
+        $x("//tr[3]/td[2]").shouldHave(text("Female"));
+        $x("//tr[4]/td[2]").shouldHave(text("8909105555"));
+        $x("//tr[5]/td[2]").shouldHave(text("18 April,1990"));
+        $x("//tr[6]/td[2]").shouldHave(text("Arts"));
+        $x("//tr[7]/td[2]").shouldHave(text("Reading"));
+        $x("//tr[8]/td[2]").shouldHave(text("file.txt"));
+        $x("//tr[9]/td[2]").shouldHave(text("Test st., 6"));
+        $x("//tr[10]/td[2]").shouldHave(text("NCR Delhi"));
+        $("#closeLargeModal").click();
         }
 }
