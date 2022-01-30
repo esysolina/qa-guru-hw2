1db8d1aa5ed1f6e8144add588a27bc7f716685e0
esysolina <evelina.ova@gmail.com>
2022-01-30 16:20:30 +0500

    dded attachments

--- src/test/java/guru/qa/tests/FormTest.java
+++ src/test/java/guru/qa/tests/FormTest.java
@@ -11,6 +11,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.files.DownloadActions.click;
 import static guru.qa.tests.TestData.*;
+import static io.qameta.allure.Allure.step;
 
 import com.github.javafaker.Faker;
 
@@ -22,33 +23,62 @@ public class FormTest extends TestBase {
 
     @Test
     void fillFormTest() {
-        registrationPage.openPage()
-                .typeFirstName(testData.firstName)
-                .typeLastName(testData.lastName)
-                .typeUserEmail(testData.userEmail)
-                .selectGender()
-                .typeUserNumber(testData.userNumber)
-                .selectDate("18", "April", "1990")
-                .typeSubject(testData.subject)
-                .selectSubject()
-                .selectHobbies()
-                .choosePicture(testData.pictureName)
-                .typeCurrentAddress(testData.currentAddress)
-                .selectState()
-                .chooseCity(testData.firstCity)
-                .selectCity()
-                .chooseCity(testData.secondCity)
-                .clickSubmitButton()
-                .checkResultsValue("Student Name", testData.firstName + " " + testData.lastName)
-                .checkResultsValue("Student Email", testData.userEmail)
-                .checkResultsValue("Gender", "Female")
-                .checkResultsValue("Mobile", testData.userNumber)
-                .checkResultsValue("Date of Birth", "18 April,1990")
-                .checkResultsValue("Subjects", "Arts")
-                .checkResultsValue("Hobbies", "Reading")
-                .checkResultsValue("Picture", testData.pictureName)
-                .checkResultsValue("Address", testData.currentAddress)
-                .checkResultsValue("State and City", testData.firstCity + " " + testData.secondCity)
-                .clickCloseButton();
+        step("Открываем страницу", () ->{
+            registrationPage.openPage();
+        });
+        step("Вводим имя", () ->{
+            registrationPage.typeFirstName(testData.firstName);
+        });
+        step("Вводим фамилию", () ->{
+            registrationPage.typeLastName(testData.lastName);
+        });
+        step("Вводим емаил", () ->{
+            registrationPage.typeUserEmail(testData.userEmail);
+        });
+        step("Выбираем пол", () ->{
+            registrationPage.selectGender();
+        });
+        step("Вводим номер", () ->{
+            registrationPage.typeUserNumber(testData.userNumber);
+        });
+        step("Выбираем дату", () ->{
+            registrationPage.selectDate("18", "April", "1990");
+        });
+        step("Вводим предмет", () ->{
+            registrationPage.typeSubject(testData.subject)
+                    .selectSubject();
+        });
+        step("Выбираем хобби", () ->{
+            registrationPage.selectHobbies();
+        });
+        step("Загружаем картинку", () ->{
+            registrationPage.choosePicture(testData.pictureName);
+        });
+        step("Вводим адрес", () ->{
+            registrationPage.typeCurrentAddress(testData.currentAddress);
+        });
+        step("Выбираем штат", () ->{
+            registrationPage.selectState()
+                    .chooseCity(testData.firstCity);
+        });
+        step("Выбираем город", () ->{
+            registrationPage.selectCity()
+                    .chooseCity(testData.secondCity);
+        });
+        step("Подтверждаем заполнение формы", () ->{
+            registrationPage.clickSubmitButton();
+        });
+        step("Проверяем заполненные данные", () ->{
+            registrationPage.checkResultsValue("Student Name", testData.firstName + " " + testData.lastName)
+                    .checkResultsValue("Student Email", testData.userEmail)
+                    .checkResultsValue("Gender", "Female")
+                    .checkResultsValue("Mobile", testData.userNumber)
+                    .checkResultsValue("Date of Birth", "18 April,1990")
+                    .checkResultsValue("Subjects", "Arts")
+                    .checkResultsValue("Hobbies", "Reading")
+                    .checkResultsValue("Picture", testData.pictureName)
+                    .checkResultsValue("Address", testData.currentAddress)
+                    .checkResultsValue("State and City", testData.firstCity + " " + testData.secondCity);
+        });
     }
 }
--- src/test/java/guru/qa/tests/TestBase.java
+++ src/test/java/guru/qa/tests/TestBase.java
@@ -1,13 +1,36 @@
 package guru.qa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import guru.qa.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.startMaximized = true;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
     }
 }
