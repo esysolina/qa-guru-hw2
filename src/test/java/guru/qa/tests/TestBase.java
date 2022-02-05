bb55de95df41e15777933e0f3272897a13f46212
esysolina <32612574+esysolina@users.noreply.github.com>
2022-02-05 16:21:52 +0500

    return closeWebDriver in the end

--- src/test/java/guru/qa/tests/TestBase.java
+++ src/test/java/guru/qa/tests/TestBase.java
@@ -31,7 +31,7 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        Selenide.closeWebDriver();
         Attach.addVideo();
+        Selenide.closeWebDriver();
     }
 }
